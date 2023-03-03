caf10b576fc407034e498046f5e6bc1cdd776380
Hayden Grubb <86503901+HaydenGrubb@users.noreply.github.com>
2023-03-03 16:48:08 -0500

    Add files via upload

---
+++ app/problems/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+//problem page uses (components)/Card for loading card
+//API call to pages\api/hello.ts for Card info to fill
+import Card from './(components)/Card'
+import 'bootstrap/dist/css/bootstrap.css';
+import "./(styles)/cardStyle.css"
+import {useState, useEffect} from "react";
+import {CardResponseType} from "@/common/types/CardResponseType";
+import axios from "axios";
+
+export default function Home() {
+    const [cardResponse, setCardResponse] = useState<CardResponseType[]>();
+
+    useEffect(() => {
+        getCardInfo();
+    }, [])
+
+    const getCardInfo = async () => {
+        await axios.get<CardResponseType[]>('http://localhost:3000/api/hello').then((response) => {
+            setCardResponse(response.data);
+        })
+    }
+
+    return (
+        <main>
+            <h1>
+                Programming Principles Tool
+            </h1>
+
+            <div className="container">
+                {cardResponse !== undefined && cardResponse.map((data, index: number) => {
+                    return (
+                        <div className="col" key={index} >
+                            <Card problemId={data.problemId} name={data.name} prompt={data.prompt}/>
+                        </div>)
+                })}
+
+            </div>
+
+
+        </main>
+    )
+}
---
+++ pages/api/hello.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  res.status(200).json([{ "problemId": 1, "name": 'Initialize Integer', "prompt": 'Displays an integer saved to a variable to ensure correct functioning.',"number": '1'}
+    , { "problemId": 2, name: 'Initialize Double', prompt: 'Displays a double saved to a variable to ensure correct functioning.', number: "2"}
+  ,{ "problemId": 3, name: 'Say Hello', prompt: 'Please print "Hello World" to the console.', number: "3"}
+  ,{ "problemId": 4, name: 'Count to Three', prompt: 'Please print the numbers 1-3 line-by-line using a for loop.', number: "4"}
+  ,{ "problemId": 5, name: 'Say Goodbye', prompt: '"Goodbye World" to the console.', number: "5"}
+  ,{ "problemId": 6, name: 'Nested For Loop', prompt: 'Testing nested for loop.', number: "6"}]);
+}
